5d83c948fce4a06aeaa8580442048a2bf5fb7e99
Neha Navgale <nn78d@mail.umkc.edu>
2019-11-30 14:23:41 -0600

    Added code for one to one chat conversion

---
+++ WebLab2/app/src/main/java/com/example/mywhatsapp/ChatConversationActivity.java
@@ -0,0 +1,202 @@
+package com.example.mywhatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class ChatConversationActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ImageButton imagebutton;
+    private EditText editText;
+    private ScrollView scrollView;
+    private TextView textView;
+    private FirebaseAuth mAuth;
+    private  String currentGroup,currentUserid,currentUserName,currentdate,currenttime;
+    private DatabaseReference dbRef,groupdbref,groupmessagekeyref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatconversation);
+
+        currentGroup = getIntent().getExtras().get("chatName").toString();
+        Toast.makeText(ChatConversationActivity.this,currentGroup,Toast.LENGTH_SHORT);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserid = mAuth.getCurrentUser().getUid();
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        groupdbref = FirebaseDatabase.getInstance().getReference().child("ChatsList").child(currentGroup);
+
+        InitializeFields();
+
+        GetuserInfo();
+
+        imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMessagetoDb();
+                editText.setText("");
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        groupdbref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if(dataSnapshot.exists())
+                {
+                    DisplayMessages(dataSnapshot);
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                if(dataSnapshot.exists())
+                {
+                    DisplayMessages(dataSnapshot);
+                }
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void InitializeFields() {
+
+        toolbar= (Toolbar) findViewById(R.id.single_chat_layout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(currentGroup);
+
+        imagebutton = (ImageButton) findViewById(R.id.send_m);
+        editText = (EditText) findViewById(R.id.input_g_message);
+        textView = (TextView) findViewById(R.id.single_chat_text_display);
+        scrollView = (ScrollView) findViewById(R.id.Scroll_View);
+    }
+
+
+    private void GetuserInfo() {
+
+        dbRef.child(currentUserid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists())
+                {
+                    currentUserName = dataSnapshot.child("name").getValue().toString();
+
+                }
+                else
+                {
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveMessagetoDb() {
+        String message = editText.getText().toString();
+        String messageKEY = groupdbref.push().getKey();
+
+        if(TextUtils.isEmpty(message))
+        {
+            Toast.makeText(this, "Please Enter Message...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Calendar currentDate = Calendar.getInstance();
+            SimpleDateFormat currentDateFormat = new SimpleDateFormat("MMM dd,yyyy");
+            currentdate = currentDateFormat.format(currentDate.getTime());
+
+
+            Calendar currentTime = Calendar.getInstance();
+            SimpleDateFormat currentTimeFormat = new SimpleDateFormat("hh:mm a");
+            currenttime = currentTimeFormat.format(currentTime.getTime());
+
+            HashMap<String,Object>  grpmessagekey = new HashMap<>();
+            groupdbref.updateChildren(grpmessagekey);
+            groupmessagekeyref = groupdbref.child(messageKEY);
+
+            HashMap<String,Object> messageInfoMap = new HashMap<>();
+            messageInfoMap.put("name",currentUserName);
+            messageInfoMap.put("message",message);
+            messageInfoMap.put("date",currentdate);
+            messageInfoMap.put("time",currenttime);
+
+            groupmessagekeyref.updateChildren(messageInfoMap);
+        }
+    }
+
+    private void DisplayMessages(DataSnapshot dataSnapshot) {
+
+        Iterator iter = dataSnapshot.getChildren().iterator();
+
+        while(iter.hasNext())
+        {
+            String chatDate = (String) ((DataSnapshot)iter.next()).getValue();
+            String chatMessage = (String) ((DataSnapshot)iter.next()).getValue();
+            String chatName = (String) ((DataSnapshot)iter.next()).getValue();
+            String chatTime = (String) ((DataSnapshot)iter.next()).getValue();
+
+            textView.append(chatName + " :\n" + chatMessage + ":\n" + chatTime + "    "+ chatDate + "\n\n\n");
+
+            scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+        }
+
+    }
+
+
+}
--- WebLab2/app/src/main/java/com/example/mywhatsapp/ChatsFragment.java
+++ WebLab2/app/src/main/java/com/example/mywhatsapp/ChatsFragment.java
@@ -9,12 +9,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.content.Intent;
+import android.icu.text.Edits;
+
+import androidx.annotation.NonNull;
+
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatsFragment extends Fragment {
 
+    private View chatsview;
+    private ListView chatlistView;
+    private ArrayAdapter<String> listaddapter;
+    private ArrayList<String> list_of_chats = new ArrayList<>();
+    private DatabaseReference dbRef;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -25,7 +51,54 @@ public class ChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        chatsview = inflater.inflate(R.layout.fragment_chats, container, false);
+        dbRef = FirebaseDatabase.getInstance().getReference().child("ChatsList");
+        InitializeFields();
+        DisplayGroups();
+
+        chatlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String CurrentChat = parent.getItemAtPosition(position).toString();
+                Intent chatIntent = new Intent(getContext(),ChatConversationActivity.class);
+                chatIntent.putExtra("chatName",CurrentChat);
+                startActivity(chatIntent);
+            }
+        });
+
+        return  chatsview;
+    }
+
+    private void InitializeFields() {
+        chatlistView = (ListView) chatsview.findViewById(R.id.chat_list);
+        listaddapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,list_of_chats);
+        chatlistView.setAdapter(listaddapter);
+    }
+    private void DisplayGroups() {
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<>();
+                Iterator iter = dataSnapshot.getChildren().iterator();
+                while(iter.hasNext()) {
+
+                    set.add(((DataSnapshot) iter.next()).getKey());
+                }
+
+                list_of_chats.clear();
+                list_of_chats.addAll(set);
+                listaddapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
